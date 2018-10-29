04f22dd448491e5783ff37f9396c3a56c136eea4
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-10-29 12:56:47 -0400

    Add files via upload

---
+++ isSelfDivisor.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class isSelfDivisor here.
+ *
+ * @author Emily Wilber
+ * @version Oct 29, 2018
+ */
+public class isSelfDivisor
+{
+    
+    /**
+     * Constructor for objects of class isSelfDivisor
+     */
+    public static boolean isSelfDivisor(int number)
+    {
+        
+    }
+}
